f7f5b1c82fd39655137bc54c0e90d8da47686d87
Robin Gordijn <robingordijn@gmail.com>
2019-04-05 17:22:11 +0200

    Updates

---
+++ precache-manifest.766c69b04244b1ad1f11ac00578a4ea1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f9508d30eef6616978d6",
+    "url": "/static/js/main.49822441.chunk.js"
+  },
+  {
+    "revision": "607320d00d64b68e18da",
+    "url": "/static/js/2.2050718e.chunk.js"
+  },
+  {
+    "revision": "f9508d30eef6616978d6",
+    "url": "/static/css/main.a9c9d87a.chunk.css"
+  },
+  {
+    "revision": "0a26a6a661971880f1a6510a379ad886",
+    "url": "/index.html"
+  }
+];
